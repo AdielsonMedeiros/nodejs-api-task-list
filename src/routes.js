10c6137f22c1231e6071459aa8659581ab4f8845
Adielson Medeiros <medeirosadielson@gmail.com>
2023-07-24 22:01:48 -0300

    acessando o database e cadastrando

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
+import User from './app/models/User';
 
 
 
 const routes = new Router();
 
-routes.get('/teste', (req, res) => {
-  return res.json({ ok: true})
+routes.get('/teste', async (req, res) => {
+  const user = await User.create({
+    name: 'ugabuga',
+    email: 'fortnite@fortnite.com',
+  password_hash: 'coxinha69',
+  })
+  return res.json(user);
 })
 
 export default routes;
