2b678a7e33a52212a21c0b4bcd12762bffa08d92
Adielson Medeiros <medeirosadielson@gmail.com>
2023-07-29 21:15:41 -0300

    validação e a crianção do userController

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,12 @@
 import { Router } from "express";
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 
 
 const routes = new Router();
 
-routes.get('/teste', async (req, res) => {
-  const user = await User.create({
-    name: 'ugabuga',
-    email: 'fortnite@fortnite.com',
-  password_hash: 'coxinha69',
-  })
-  return res.json(user);
-})
+routes.post('/users', UserController.store);
+
+
 
 export default routes;
