b4d06f977270e56cc0a9a65420a02b22a569e368
Adielson Medeiros <medeirosadielson@gmail.com>
2023-08-07 16:19:50 -0300

    utilizando middleware de autenticaçãon

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,16 @@ import SessionController from './app/controllers/SessionController';
 
 
 const routes = new Router();
+import authMiddleware from './app/middlewares/auth';
 
 routes.post('/users', UserController.store);
+
 routes.post('/sessions', SessionController.store);
+
+//todas as rotas abaixo desse middleware precisam estar autenticado
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 
 
